1fb74451f7802aa15ecd5351e9240b57fc9660e5
shuc0019 <shuc0019@stud.kea.dk>
2023-10-06 13:03:05 +0200

    rediger ent

--- KinoXpProjects/src/main/java/com/example/kinoxpproject/entity/Seat.java
+++ KinoXpProjects/src/main/java/com/example/kinoxpproject/entity/Seat.java
@@ -24,11 +24,4 @@ public class Seat {
     @ManyToOne
     @JoinColumn(name = "hall_id")
     private Hall hall;
-
-    @Column(name = "is_booked") // Specify the column name for isBooked
-    private boolean isBooked;
-
-    @ManyToOne
-    @JoinColumn(name = "show_id") // Add this to associate a seat with a show
-    private Shows show;
 }
